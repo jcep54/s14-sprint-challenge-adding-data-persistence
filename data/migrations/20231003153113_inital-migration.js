7c0a7627d6d9349884250492996f8227d1aedf10
Jose Cepero <ceperojose54@gmail.com>
2023-10-03 14:24:10 -0400

    wip

--- data/migrations/20231003153113_inital-migration.js
+++ data/migrations/20231003153113_inital-migration.js
@@ -6,6 +6,11 @@ exports.up = async function(knex) {
   await knex.schema
   .createTable('projects', table =>{
     table.increments('project_id')
+    table.string('project_name')
+        .notNullable()
+    table.string('prject_description')
+    table.boolean('project_completed')
+        .defaultTo(false)
   })
 };
 
