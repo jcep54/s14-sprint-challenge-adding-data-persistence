cc1d7323308804d845d3872b960ed783f76122aa
Jose Cepero <ceperojose54@gmail.com>
2023-10-03 15:15:29 -0400

    wip finished migration

--- data/migrations/20231003153113_inital-migration.js
+++ data/migrations/20231003153113_inital-migration.js
@@ -12,6 +12,47 @@ exports.up = async function(knex) {
     table.boolean('project_completed')
         .defaultTo(false)
   })
+  .createTable('resources', table =>{
+    table.increments('resource_id')
+    table.string('resource_name')
+        .notNullable()
+        .unique()
+    table.string('resource_description')
+  })
+  .createTable('tasks', table =>{
+    table.increments('task_id')
+    table.string('task_description')
+        .notNullable()
+    table.string('task_notes')
+    table.boolean('task_completed')
+        .defaultTo(false)
+    table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+  })
+  .createTable('project_resources', table =>{
+    table.increments('project_resource_id')
+    table.integer('resource_id')
+        .unsigned()
+        .notNullable()
+        .references('resource_id')
+        .inTable('resources')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+    table.integer('project_id')
+        .unsigned()
+        .notNullable()
+        .references('project_id')
+        .inTable('projects')
+        .onDelete('RESTRICT')
+        .onUpdate('RESTRICT')
+
+
+  })
 };
 
 /**
@@ -21,4 +62,6 @@ exports.up = async function(knex) {
 exports.down = async function(knex) {
   await knex.schema
   .dropTableIfExists('projects')
+  .dropTableIfExists('resources')
+  .dropTableIfExists('tasks')
 };
